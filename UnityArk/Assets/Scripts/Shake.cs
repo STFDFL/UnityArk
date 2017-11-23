d0cac0e246c4d3e858ad80678d42de25c4ee8489
Stef <stedeflo@hotmail.com>
2017-11-23 21:57:44 +1100

    Added Shake scene

---
+++ UnityArk/Assets/Scripts/Shake.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shake : MonoBehaviour {
+
+
+    // Transform of the camera to shake. Grabs the gameObject's transform
+    // if null.
+    public Transform camTransform;
+
+    // How long the object should shake for.
+    public float shakeDuration = 0f;
+
+    // Amplitude of the shake. A larger value shakes the camera harder.
+    public float shakeAmount = 0.7f;
+    public float decreaseFactor = 1.0f;
+
+    Vector3 originalPos;
+
+    void Awake()
+    {
+        if (camTransform == null)
+        {
+            camTransform = GetComponent(typeof(Transform)) as Transform;
+        }
+    }
+
+    void OnEnable()
+    {
+        originalPos = camTransform.localPosition;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            shakeDuration = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            shakeDuration = 3;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            shakeDuration = 3;
+        }
+
+
+        if (shakeDuration > 0)
+        {
+            camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+
+            shakeDuration -= Time.deltaTime * decreaseFactor;
+        }
+        else
+        {
+            shakeDuration = 0f;
+            camTransform.localPosition = originalPos;
+        }
+    }
+
+
+}
