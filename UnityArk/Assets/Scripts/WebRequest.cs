fd42c7285d5f9e9e5a052cc309100ace28e632e7
Stef <stedeflo@hotmail.com>
2018-01-04 15:01:20 +1100

    Added Unity Web Request Scene

---
+++ UnityArk/Assets/Scripts/WebRequest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class WebRequest : MonoBehaviour {
+
+    void Start()
+    {
+        StartCoroutine(GetText());
+    }
+
+    IEnumerator GetText()
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get("http://unity3d.com/"))
+        {
+            yield return request.Send();
+
+            if (request.isNetworkError) // Error
+            {
+                Debug.Log("Error: " + request.error);
+            }
+            else // Success
+            {
+                Debug.Log("Success: " + request.downloadHandler.text);
+            }
+        }
+    }
+}
